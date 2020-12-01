af2c96207bdfcb04e02e9e78c6b7dbef3c88f5d6
Hevia <anthony@hevia.dev>
2020-12-01 15:52:05 -0500

    API code & boilerplate frontend code

--- api/main.py
+++ api/main.py
@@ -1,2 +1,45 @@
-# empty code for FastAPI stuff later
-#
+from typing import Dict
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from .model import get_model, Model
+from .article_scraper import scrapeArticle
+app = FastAPI()
+
+class ClassifierRequest(BaseModel):
+    url: str
+
+class ClassifierResponse(BaseModel):
+    probabilities: Dict[str, float]
+    sentiment: str
+    confidence: float
+    message: str
+
+@app.post("/predictTitle")
+def predictTitle(request: ClassifierRequest, model: Model = Depends(get_model)):
+    try:
+        title, text = scrapeArticle(request.url)
+        sentiment, confidence, probabilities = model.predict(title)
+        return ClassifierResponse(
+        sentiment=sentiment, confidence=confidence, probabilities=probabilities, message="Success!"
+    )
+    except Exception as e:
+        return ClassifierResponse(
+        sentiment="null", confidence=0.0, probabilities={"null": 0.0}, message=f"Error scraping article: {e}"
+    )
+    
+    
+
+# Dependency Injection thanks to FastAPI
+@app.post("/predictArticle", response_model=ClassifierResponse)
+def predictArticle(request: ClassifierRequest, model: Model = Depends(get_model)):
+    try:
+        title, text = scrapeArticle(request.url)
+        sentiment, confidence, probabilities = model.predict(text)
+        return ClassifierResponse(
+        sentiment=sentiment, confidence=confidence, probabilities=probabilities, message="Success!"
+    )
+    except Exception as e:
+        return ClassifierResponse(
+        sentiment="null", confidence=0.0, probabilities={"null": 0.0}, message=f"Error scraping article: {e}"
+    )
+    
---
+++ frontend/src/SearchBar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./App.css";
+import { DarkTheme, useStyletron } from "baseui";
+import { Input, SIZE } from "baseui/input";
+import { Button } from "baseui/button";
+import axios from "axios";
+
+function SearchBar(props) {
+  const [value, setValue] = React.useState("");
+  const [css, theme] = useStyletron();
+  const API_ENDPOINT = ""
+
+  const handleKeyPress = (event) => {
+    if (value !== "" && event.key === "Enter") {
+      event.preventDefault();
+      makeSearchRequest(value);
+    }
+  };
+
+  async function makeSearchRequest(user_input) {
+    const response = await axios.post(
+      API_ENDPOINT,
+      { search_query: user_input },
+      { headers: { "Content-Type": "application/json" } }
+    );
+    Promise.resolve(response).then(() => {
+      console.log("Promise completed....")
+      console.log(response.data)
+      props.stateContext.searchResults.set(response.data.search_results);
+      props.stateContext.resultsFound.set(true);
+    });
+  }
+
+  return (
+    <div>
+      <svg className="homepage-logo"></svg>
+      <div className="homepage-body">
+        <div className="homepage-search-bar">
+          <Input
+            theme={DarkTheme}
+            className="search-bar"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            size={SIZE.default}
+            placeholder="Paste article URL here."
+            clearOnEscape
+            onKeyDown={handleKeyPress}
+            overrides={{
+              Root: {
+                style: {
+                  width: "100%",
+                  marginRight: theme.sizing.scale300,
+                },
+              },
+            }}
+          />
+          <Button
+            className="search-button"
+            onClick={async () => await makeSearchRequest(value)}
+          >
+            Classify
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
