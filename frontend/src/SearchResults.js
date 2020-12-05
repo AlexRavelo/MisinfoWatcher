dd10dc4ddf39823b5c7e3a9976b028c5fa258596
Hevia <anthony@hevia.dev>
2020-12-04 23:45:59 -0500

    cleaning up code

--- frontend/src/SearchResults.js
+++ frontend/src/SearchResults.js
@@ -1,44 +1,39 @@
 import * as React from "react";
 import './App.css';
-import {
-  Card,
-  StyledAction
-} from "baseui/card";
-import { Button } from "baseui/button";
 
 function SearchResults(props) {
-  return (
+return (
 <div>
-  <a href="/"><svg className="search-results-logo"></svg></a>
-  <div className="search-result-box">
-    <div>
+    <a href="/"><svg className="search-results-logo"></svg></a>
+    <div className="search-result-box">
         <div>
-            <h1>
-                MisinfoWatcher believes the article text is: {props.stateContext.searchResults.get.article_sentiment} 
-                with a confidence of {props.stateContext.searchResults.get.article_confidence}%
-            </h1>
-        </div>
-            
-        <div>
-            <h1>
-                MisinfoWatcher believes the article title is: {props.stateContext.searchResults.get.title_sentiment} 
-                with a confidence of {props.stateContext.searchResults.get.title_confidence}%
-            </h1>
-        </div>
+            <div>
+                <h1>
+                    MisinfoWatcher believes the article text is: {props.stateContext.searchResults.get.article_sentiment} 
+                    with a confidence of {props.stateContext.searchResults.get.article_confidence}%
+                </h1>
+            </div>
+                
+            <div>
+                <h1>
+                    MisinfoWatcher believes the article title is: {props.stateContext.searchResults.get.title_sentiment} 
+                    with a confidence of {props.stateContext.searchResults.get.title_confidence}%
+                </h1>
+            </div>
 
-        <div>
-            <ul>
-                {
-                    props.stateContext.searchResults.get.entities.map(entity => (
-                    <li>{entity}</li>
-                    ))
-                }
-            </ul>
+            <div>
+                <ul>
+                    {
+                        props.stateContext.searchResults.get.entities.map(entity => (
+                        <li>{entity}</li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
-    </div>
     </div>
 </div>
-  );
+);
 }
 
 export default SearchResults;
