0ed0f2da2559a75d0f485eb75c4b637088bf9517
Alex Ravelo <alexandooop@gmail.com>
2020-12-06 19:36:17 -0500

    Added naive bayes classifier

--- article-scraper.py
+++ article-scraper.py
@@ -3,16 +3,30 @@ import urllib
 import time
 from typing import Tuple
 from newspaper import Article
+<<<<<<< Updated upstream
 
 def scrapeArticle(url) -> Tuple[str, str]:
+=======
+def scrapeArticle(url: str) -> Tuple[str, str]:
+	
+>>>>>>> Stashed changes
     # nabbing the article
     article = Article(url)
     article.download()
     article.html
     article.parse()
+<<<<<<< Updated upstream
     
     # nabbing both the text and the title
     article_text = article.text
     title_text = article.title
     
     return title_text, article_text
+=======
+
+    # nabbing both the text and the title
+    article_text = article.text
+    title_text = article.title
+
+	return title_text, article_text
+>>>>>>> Stashed changes
